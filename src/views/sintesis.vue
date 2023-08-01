a2610465bdd8d3ce01616deb26de4ae36992251b
Mauricio Otalora <cmotalora@sena.edu.co>
2023-07-31 22:47:47 -0500

    sintesis6

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,12 +3,14 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p A continuación, se describe el tema principal del componente formativo La huella hídrica, en donde el agua es catalogada como fuente de vida y un recurso fundamental para el desarrollo de la humanidad, pero la misma humanidad se ha encargado de malgastarla y contaminarla. Por tanto, en la búsqueda de mejorar la gestión, aprovechamiento y uso racional que permita alcanzar el desarrollo sostenible de éste recurso natural, surgen métodos como la huella hídrica que permite determinar el verdadero volumen de agua empleado para la producción de bienes y prestación de servicios, pero con factor especial que no sólo cuantifica la cantidad de agua consumida y contaminada, sino que también realiza la evaluación de impactos y prioriza medidas y acciones para minimizarlos. 
+    p El agua es catalogada como fuente de vida y un recurso fundamental para el desarrollo de la humanidad, pero la misma humanidad se ha encargado de malgastar y contaminar el agua, siendo de vital importancia tomar conciencia sobre el impacto que genera el estilo de vida, los productos que se consumen y las formas de producción tienen sobre el agua. Por tanto, en la búsqueda de mejorar la gestión, realizando un aprovechamiento y uso racional que permita alcanzar el desarrollo sostenible de éste recurso natural, surgen métodos como la huella hídrica que permite determinar el verdadero volumen de agua empleado para la producción de bienes y prestación de servicios, pero con factor especial que no sólo cuantifica la cantidad de agua consumida y contaminada, sino que también realiza la evaluación de impactos y prioriza medidas y acciones para minimizarlos.
+
+    p Así pues, un resumen de lo visto en el presente componente, podrá ser visualizado en el siguiente mapa conceptual:
 
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="sintesis")
       .col-auto
         a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
