d5b60a0b10476c6c60f1e696e60e64687571651d
Mauricio Otalora <cmotalora@sena.edu.co>
2023-07-31 22:08:49 -0500

    sintesis, glosario, referencias, ajustes portada, algunos textos alternativos terminado

--- src/config/global.js
+++ src/config/global.js
@@ -4,7 +4,7 @@ export default {
     descripcionCurso:
       'En este componente formativo se abordan el agua y algunas temáticas que permitirán comprender de forma general la evolución del concepto de huella hídrica y su normatividad, además de los componentes, tipos y metodologías existentes para su medición.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
-    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
+    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.svg'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
@@ -135,32 +135,121 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'El agua',
+      referencia:
+        'Ministerio de ambiente, vivienda y desarrollo territorial. (2010). Política Nacional para la Gestión Integral del Recurso Hídrico. Ministerio de Ambiente, Vivienda y Desarrollo Territorial. ',
+      tipo: 'Documento',
+      link:
+        'https://www.minambiente.gov.co/wp-content/uploads/2021/10/Politica-nacional-Gestion-integral-de-recurso-Hidrico-web.pdf',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Evaluación multisectorial de la huella hídrica en Colombia',
+      referencia:
+        'Campuzano, C.; González Valencia, J.; Guzmán Cabrera, J.; Rodríguez Ortiz, C.; Arévalo Uribe, D.; Parada Puig, G.; Zárate Torres, E.; Kuiper, D. (2015) Evaluación multisectorial de la huella hídrica en Colombia. Resultados por subzonas hidrográficas en el marco del Estudio Nacional del Agua. CTA, GSILAC, COSUDE and IDEAM. ',
+      tipo: 'Documento',
+      descarga:
+        'http://documentacion.ideam.gov.co/openbiblio/bvirtual/023272/HH_ENA2014.pdf ',
+    },
+    {
+      tema:
+        'Metodología para cálculo de la huella hídrica                                                                ',
+      referencia:
+        'Agualimpia, O. N. G., Andina, P. S. (2016). Manual de aplicación de evaluación de huella hídrica acorde a la norma ISO 14046.',
+      tipo: 'Documento',
+      descarga:
+        'https://fch.cl/wp-content/uploads/2019/12/manual-aplicacion-iso-14-046-suizagua-1.pdf ',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Ciclo de vida:',
+      significado:
+        'etapas consecutivas e interrelacionadas de un sistema del producto, desde la adquisición de materia prima o de su generación a partir de recursos naturales hasta la disposición final.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Ciclo hidrológico:',
+      significado:
+        'conjunto continuo de etapas y cambios que atraviesa el agua en la naturaleza.',
+    },
+    {
+      termino: 'Huella de Agua:',
+      significado:
+        'cuantificación de los impactos del uso del agua desde el análisis del ciclo de vida. ',
+    },
+    {
+      termino: 'Huella hídrica:',
+      significado:
+        'indicador del agua dulce usada, alterada y contaminada directa o indirectamente por la humanidad, que permite evaluar los riesgos asociados y desarrollar estrategias para mitigarlos.',
+    },
+    {
+      termino: 'Huella hídrica azul:',
+      significado:
+        'volumen de agua dulce empleada (incorporada o evaporada) en un proceso productivo proveniente de los recursos hídricos del planeta, es decir, las aguas superficiales y subterráneas. ',
+    },
+    {
+      termino: 'Huella hídrica gris:',
+      significado:
+        'volumen de agua contaminada en la producción de los bienes y servicios o requerida para diluir los contaminantes hasta obtener agua de calidad buena para el ambiente y los humanos.',
+    },
+    {
+      termino: 'Huella hídrica verde:',
+      significado:
+        'volumen de agua almacenada en el suelo como humedad proveniente de la precipitación, que no se convierte en escorrentía, y que se incorpora o se evapora en un proceso productivo. ',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Agualimpia, O. N. G., Andina, P. S. (2016). Manual de aplicación de evaluación de huella hídrica acorde a la norma ISO 14046. ',
       link: '',
+    },
+    {
+      referencia:
+        'Campuzano Ochoa, C. A., González Valencia, J. E., Guzmán Cabrera, A. C., Rodríguez Ortiz, C. M., Arévalo Uribe, D., Parada Puig, G. (2014). Evaluación multisectorial de la huella hídrica en Colombia. Resultados por subzonas hidrográficas en el marco del Estudio Nacional del Agua',
+      link: '',
+    },
+    {
+      referencia:
+        'CTA, GSILAC, COSUDE & IDEAM. Evaluación multisectorial de la huella hídrica en Colombia. Resultados por subzonas hidrográficas en el marco del Estudio Nacional del Agua 2014, Medellín, Bogotá. [En línea]. 2015',
+      link: '',
+    },
+    {
+      referencia:
+        'Cirelli, A. F. (2012). El agua: un recurso esencial. Química viva, 11(3), 147-170.',
+      link: '',
+    },
+    {
+      referencia:
+        'Hoekstra, A. Y., Chapagain, A. K., Mekonnen, M. M., & Aldaya, M. M. (2011). The water footprint assessment manual: Setting the global standard. Routledge.',
+      link: '',
+    },
+    {
+      referencia: 'IDEAM. (2015). Estudio Nacional del Agua 2014. IDEAM.',
+      link: '',
+    },
+    {
+      referencia:
+        'ISO 14046:2014 Environmental management -- Water footprint -- Principles, requirements and guidelines. En línea https://www.iso.org/obp/ui#iso:std:iso:14046:ed-1:v1:es',
+      link: '',
+    },
+    {
+      referencia:
+        'Mazari-Hiriart, M. (2003). El agua como recurso. Cómo ves, 54, 10-12.',
+      link: '',
+    },
+    {
+      referencia:
+        'Martínez, A., Chargoy, J., Puerto, M., Suppen, N., Rojas, D., & Villarraga, J. (2016). Huella de Agua (ISO 14046) en América Latina, análisis y recomendaciones para una coherencia regional. Centro de Análisis de Ciclo de Vida y Diseño Sustentable CADIS, Embajada de Suiza en Colombia, Agencia Suiza para la Cooperación y el Desarrollo COSUDE, 90.',
+      link: '',
+    },
+    {
+      referencia:
+        'Schneider, H., & Samaniego, J. (2009). La huella del carbono en la producción, distribución y consumo de bienes y servicios.  Comisión Económica para América Latina y el Caribe (CEPAL), 29-34.',
+      link: '',
+    },
+    {
+      referencia: 'World Wildlife Fund (WWF) (2008), Informe Planeta Vivo. ',
+      link: 'http://assets.panda.org/downloads/lpr_2008_span_lo_res.pdf',
     },
   ],
   creditos: [
@@ -198,7 +287,7 @@ export default {
           centro: 'Nombre centro de formación',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Cristian Mauricio Otálora Clavijo',
           cargo: 'Desarrollador Fullstack',
           centro: 'Nombre centro de formación',
         },
